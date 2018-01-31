4b60a9fec7aab42d5c90e3542abdb5ae99a69a31
csongi77 <horvath.csongor@outlook.com>
2018-01-31 14:40:27 +0100

    add autolink to phone and web

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.android.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.phone_call);
+    /*   for telephone number 650-555-5555 :)
+
+            textView.setOnClickListener(v -> {
+            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+            String toCall = textView.getText().toString();
+            callIntent.setData(Uri.parse("tel:"+toCall));
+            startActivity(callIntent);
+        });*/
     }
 }
